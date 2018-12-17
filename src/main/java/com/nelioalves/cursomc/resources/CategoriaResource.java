8bfb01caf1560b1db77dd49565d0f4b0f060d774
Rodrigo Prado <digosilva.prado@gmail.com>
2018-12-17 09:00:48 -0200

    Inclusao Repository e Service

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,29 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService categoriaService;
 
-		Categoria ct1 = new Categoria(1, "Rodrigo");
-		Categoria ct2 = new Categoria(2, "Paulo");
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(ct1);
-		lista.add(ct2);
+		Categoria categoriaRetorno = categoriaService.buscar(id);
 
-		return lista;
+		return new ResponseEntity<>(categoriaRetorno, HttpStatus.OK);
 	}
 
 }
